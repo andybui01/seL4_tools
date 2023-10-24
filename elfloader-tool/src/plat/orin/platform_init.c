62e3493878132f1364d19f69d0fd43c2c69be80f
Andy Bui <andy.bui@nio.io>
2023-10-24 17:40:14 +1100

    elfloader: add support for Jetson Orin
    This commit also adds support for the tegra194-tcu uart device.
    
    Signed-off-by: Andy Bui <andy.bui@nio.io>

---
+++ elfloader-tool/src/plat/orin/platform_init.c
@@ -0,0 +1,19 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
+#include <autoconf.h>
+#include <mode/arm_generic_timer.h>
+
+/* Reset the virtual offset for the platform timer to 0 */
+void platform_init(void)
+{
+    reset_cntvoff();
+}
+
+#if CONFIG_MAX_NUM_NODES > 1
+void non_boot_init(void)
+{
+    reset_cntvoff();
+}
+#endif
